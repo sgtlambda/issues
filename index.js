56518ec36ad440675c6dbc7c11a82bc182eb1968
JM Versteeg <janmerijnversteeg@gmail.com>
2017-11-21 22:51:16 +0100

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+'use strict';
+
+const execa = require('execa');
+const opn   = require('opn');
+
+const sites = [
+    {
+        pattern: /^git@bitbucket\.org:(.*)\.git$/,
+        url:     matches => `https://bitbucket.org/${matches[1]}/issues?status=new&status=open`,
+    },
+    {
+        pattern: /git@github\.com:(.*)\.git/,
+        url:     matches => `https://github.com/${matches[1]}/issues`,
+    }
+];
+
+const getIssuesUrl = origin => {
+
+    for (const {pattern, url} of sites) {
+        const matches = pattern.exec(origin);
+        if (!matches) continue;
+        return url(matches);
+    }
+
+    return null;
+};
+
+(async () => {
+
+    let origin;
+
+    try {
+        ({stdout: origin} = await execa.shell('git remote get-url origin'));
+    } catch (e) {
+        process.stderr.write(e.stderr);
+        process.exit(e.code);
+    }
+
+    const url = getIssuesUrl(origin);
+
+    if (!url) throw new Error(`${origin}: not a github or bitbucket repository`);
+    opn(url, {wait: false});
+})();
